630a7b76b15a3cba158603d95cd72c814662cd17
CebuanoCodes <96157842+CebuanoCodes@users.noreply.github.com>
2021-12-15 14:48:10 +0800

    Add files via upload

---
+++ PaymentSystem/PaymentSystem/Models/DataManager/UserDataManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PaymentSystem.Repositories;
+
+namespace PaymentSystem.Models.DataManager
+{
+    public class UserDataManager : IDataRepository
+    {
+        readonly UserContext _userContext;
+
+        public UserDataManager(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        public  IEnumerable<User> GetAll()
+        {
+
+            var users =  _userContext.Users
+                .Include("Payments")
+                .ToList();
+
+            //// Paging
+            //int pageSize = 5;
+            //users = PaginatedList<User>.Create(users.AsQueryable(), pageNumber ?? 1, pageSize);
+
+            return users;
+        }
+        public User Get(int id)
+        {
+            return _userContext.Users
+                .Include(a => a.Payments.OrderByDescending(o => o.Date))
+                .Single(b => b.Id == id);
+        }
+
+    }
+}
---
+++ PaymentSystem/PaymentSystem/Models/Repositories/IDataRepository.cs
@@ -0,0 +1,12 @@
+﻿using PaymentSystem.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PaymentSystem.Repositories
+{
+    public interface IDataRepository
+    {
+         IEnumerable<User> GetAll();
+         User Get(int id);
+    }
+}
